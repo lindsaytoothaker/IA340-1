8d2aa7f77610dc6582a4da77889550bd3bdb3da9
lindsaytoothaker <toothalr@dukes.jmu.edu>
2024-09-18 16:40:16 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -1,1 +1,22 @@
+--4.1
+
+/* cannot delete Virginia from table because it refers to the other tables. 
+FIPS/name are forgein keys on other tables. 
+This is a 1 to M relationship
+*/
+
+--4.2
+/* we cannot paste the line of code "insert into income(fips,income,year) values('80',6000,2025)"
+because there is no state with fips 80
+*/
+
+--4.3
+/* just fine */
+
+--4.4
+/* it works perfect */
+
+--4.6
+/* it works perfect */
+
 
